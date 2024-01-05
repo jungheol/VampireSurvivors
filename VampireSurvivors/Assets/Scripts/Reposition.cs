4dc28e73b136c63d721bc5733aaadd741f7392d7
jungheol <jungheol@naver.com>
2024-01-05 21:25:07 +0900

    Monster가 플레이어로부터 멀어졌을 때 재배치되도록 구현

--- VampireSurvivors/Assets/Scripts/Reposition.cs
+++ VampireSurvivors/Assets/Scripts/Reposition.cs
@@ -2,9 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class Reposition : MonoBehaviour
-{
+public class Reposition : MonoBehaviour {
+    
+    private Collider2D coll;
+
+    private void Awake() {
+        coll = GetComponent<Collider2D>();
+    }
+
     private void OnTriggerExit2D(Collider2D other) {
         if (!other.CompareTag("Area"))
             return;
@@ -24,6 +31,11 @@ public class Reposition : MonoBehaviour
                     transform.Translate(Vector3.right * dirX * 40);
                 } else if (diffX < diffY) {
                     transform.Translate(Vector3.up * dirX * 40);
+                }
+                break;
+            case "Enemy":
+                if (coll.enabled) {
+                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0f));
                 }
                 break;
         }
