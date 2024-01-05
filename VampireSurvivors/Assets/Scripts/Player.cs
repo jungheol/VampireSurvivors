6a9b1a9dbfbe1066dad06f2cb2b697d54e348583
jungheol <jungheol@naver.com>
2024-01-05 19:46:01 +0900

    캐릭터 좌우 반전 추가

--- VampireSurvivors/Assets/Scripts/Player.cs
+++ VampireSurvivors/Assets/Scripts/Player.cs
@@ -10,14 +10,22 @@ public class Player : MonoBehaviour {
     public float moveSpeed;
 
     private Rigidbody2D rigid;
+    private SpriteRenderer spriteRenderer;
 
     private void Awake() {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate() {
         Vector2 nextVec = inputVec * moveSpeed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    private void LateUpdate() {
+        if (inputVec.x != 0) {
+            spriteRenderer.flipX = inputVec.x < 0;
+        }
     }
 
     private void OnMove(InputValue value) {
