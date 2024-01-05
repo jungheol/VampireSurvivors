a51fe862b8f4086aa539c479ca89cf18a543821c
jungheol <jungheol@naver.com>
2024-01-05 23:08:34 +0900

    Bullet을 생성해줄 Weapon 오브젝트에 스크립트 생성

---
+++ VampireSurvivors/Assets/Scripts/Weapon.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+	public int id;
+	public int prefabId;
+	public float damage;
+	public int count;
+	public float speed;
+
+	private void Start() {
+		Init();
+	}
+
+	private void Update() {
+		switch (id) {
+			case 0:
+				transform.Rotate(Vector3.back * speed * Time.deltaTime);
+				break;
+			case 1:
+				break;
+			default:
+				break;
+		}
+	}
+
+	public void Init() {
+		switch (id) {
+			case 0:
+				speed = 150;
+				Batch();		
+				break;
+			case 1:
+				break;
+			default:
+				break;
+		}
+	}
+
+	private void Batch() {
+		for (int i = 0; i < count; i++) {
+			Transform bullet = GameManager.instance.pool.Get(prefabId).transform;
+			bullet.parent = transform;
+			bullet.GetComponent<Bullet>().Init(damage, -1); // -1 은 무한관통.
+		}
+	}
+}
+
