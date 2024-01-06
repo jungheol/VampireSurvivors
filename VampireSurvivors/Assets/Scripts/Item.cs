6c24ea14b776d34c3be3f77b928a52580902d4f1
jungheol <jungheol@naver.com>
2024-01-06 16:01:28 +0900

    버튼 누르면 레벨업 하고, 레벨이 설정되어 있는 최고 레벨에 도달했을 때 버튼 누르지 못하도록 구현

--- VampireSurvivors/Assets/Scripts/Item.cs
+++ VampireSurvivors/Assets/Scripts/Item.cs
@@ -22,4 +22,24 @@ public class Item : MonoBehaviour {
     private void LateUpdate() {
         textLevel.text = "Lv." + (level + 1);
     }
+
+    public void OnClick() {
+        switch (data.itemType) {
+            case ItemData.ItemType.Melee:
+            case ItemData.ItemType.Range:
+                break;
+            case ItemData.ItemType.Glove:
+                break;
+            case ItemData.ItemType.Shoe:
+                break;
+            case ItemData.ItemType.Heal:
+                break;
+        }
+
+        level++;
+
+        if (level == data.damages.Length) {
+            GetComponent<Button>().interactable = false;
+        }
+    }
 }
