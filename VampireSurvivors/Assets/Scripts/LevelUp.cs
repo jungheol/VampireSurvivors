3c0f56cbb2ab503f2113eb0f683c089cb807add6
jungheol <jungheol@naver.com>
2024-01-06 22:50:59 +0900

    레벨업 했을 때 나오는 3가지 아이템을 랜덤(겹치지 않고, 만렙이면 나오지 않도록)으로 하는 함수 구현

--- VampireSurvivors/Assets/Scripts/LevelUp.cs
+++ VampireSurvivors/Assets/Scripts/LevelUp.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class LevelUp : MonoBehaviour {
 
@@ -13,6 +14,7 @@ public class LevelUp : MonoBehaviour {
     }
 
     public void Show() {
+        Next();
         rect.localScale = Vector3.one;
         GameManager.instance.Stop();
     }
@@ -24,5 +26,35 @@ public class LevelUp : MonoBehaviour {
 
     public void Select(int index) {
         items[index].OnClick();
+    }
+
+    private void Next() {
+        // 모든 아이템 비활성화
+        foreach (Item item in items) {
+            item.gameObject.SetActive(false);
+        }
+        // 그 중에서 랜덤하게 3개의 아이템만 활성화
+        int[] ran = new int[3];
+        while (true) {
+            ran[0] = Random.Range(0, items.Length);
+            ran[1] = Random.Range(0, items.Length);
+            ran[2] = Random.Range(0, items.Length);
+            
+            if (ran[0] != ran[1] && ran[1] != ran[2] && ran[0] != ran[2]) {
+                break;
+            }
+        }
+
+        for (int i = 0; i < ran.Length; i++) {
+            Item ranItem = items[ran[i]];
+            // 만렙 아이템을 소비아이템으로 대체
+            if (ranItem.level == ranItem.data.damageRate.Length) {
+                items[4].gameObject.SetActive(true);
+            } else {
+                ranItem.gameObject.SetActive(true);
+            }
+            
+            
+        }
     }
 }
