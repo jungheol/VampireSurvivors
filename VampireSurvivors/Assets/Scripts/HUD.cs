bbf4b5300a02db5a4d6036350fce10dbe3d6e7b8
jungheol <jungheol@naver.com>
2024-01-06 13:55:29 +0900

    UI 타이머 텍스트 구현

--- VampireSurvivors/Assets/Scripts/HUD.cs
+++ VampireSurvivors/Assets/Scripts/HUD.cs
@@ -35,6 +35,10 @@ public class HUD : MonoBehaviour {
                 myText.text = GameManager.instance.killPoint.ToString();
                 break;
             case InfoType.Time:
+                float remainTime = GameManager.instance.maxGameTime - GameManager.instance.gameTime;
+                int min = Mathf.FloorToInt(remainTime / 60);
+                int sec = Mathf.FloorToInt(remainTime % 60);
+                myText.text = string.Format("{0:D2}:{1:D2}", min, sec);
                 break;
             case InfoType.Health:
                 break;
